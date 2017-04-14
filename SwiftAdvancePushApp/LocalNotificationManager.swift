4fbb92fd02d38192fafdd49add38cad0f1b610ec
oonoyosp <oonoyosp@systena.co.jp>
2017-04-14 17:12:09 +0900

    fix Copyright name for apple doc

--- SwiftAdvancePushApp/LocalNotificationManager.swift
+++ SwiftAdvancePushApp/LocalNotificationManager.swift
@@ -3,7 +3,7 @@
 //  SwiftAdvancePushApp
 //
 //  Created by Natsumo Ikeda on 2016/07/27.
-//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//  Copyright 2017 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
 //
 
 import UIKit
